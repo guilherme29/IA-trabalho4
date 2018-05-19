9e128b2d9c8f98b45c46caf1dc96c02e5e34b8c4
Guilherme Martins Amado <gui.amado29@gmail.com>
2018-05-19 23:55:52 +0100

    Adicionada função para dar print à tabela.

--- Table.java
+++ Table.java
@@ -39,4 +39,13 @@ class Table{
         }
         return result;
     }
+
+    public void print(){
+        for(int i=0;i<table.length;i++){
+            for(int j=0;j<table[i].length;j++){
+                System.out.print(table[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
